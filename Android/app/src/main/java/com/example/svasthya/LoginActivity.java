32e635df020bc8c79ffa61c7d9a48a4da93e4340
Abhilash Saj <abhilashsaj@gmail.com>
2021-01-28 05:51:21 +0530

    completed Landing Screen

--- Android/app/src/main/java/com/example/svasthya/LoginActivity.java
+++ Android/app/src/main/java/com/example/svasthya/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.svasthya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,4 +12,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+
 }
--- Android/app/src/main/java/com/example/svasthya/MainActivity.java
+++ Android/app/src/main/java/com/example/svasthya/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.svasthya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View view) {
+    }
+
+    public void goToSignUp(View view) {
     }
 }
