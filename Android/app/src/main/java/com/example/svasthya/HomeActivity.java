9aa99543bd5f9bdecc20a3136c6cad590bb3cc3c
Abhilash Saj <abhilashsaj@gmail.com>
2021-01-29 18:17:49 +0530

    created Home Activity and Logout functionality

--- Android/app/src/main/java/com/example/svasthya/HomeActivity.java
+++ Android/app/src/main/java/com/example/svasthya/HomeActivity.java
@@ -2,7 +2,11 @@ package com.example.svasthya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(HomeActivity.this, MainActivity.class));
+        finish();
     }
 }
--- Android/app/src/main/java/com/example/svasthya/MainActivity.java
+++ Android/app/src/main/java/com/example/svasthya/MainActivity.java
@@ -6,12 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser!= null){
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
     }
 
     public void goToLogin(View view) {
