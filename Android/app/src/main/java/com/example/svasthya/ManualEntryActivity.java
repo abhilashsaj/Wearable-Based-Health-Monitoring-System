8a0cf628f2eed27ec96000a5967d67108764980c
Abhilash Saj <abhilashsaj@gmail.com>
2021-01-30 19:40:44 +0530

    created home activity layout

---
+++ Android/app/src/main/java/com/example/svasthya/ManualEntryActivity.java
@@ -0,0 +1,14 @@
+package com.example.svasthya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManualEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_entry);
+    }
+}
