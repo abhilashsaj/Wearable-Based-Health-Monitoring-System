d7271c5ff9ad4723adf1fae50aac8fd2a63a25af
Abhilash Saj <abhilashsaj@gmail.com>
2021-01-31 20:04:15 +0530

    IMEI

--- Android/app/src/main/java/com/example/svasthya/RegisterActivity.java
+++ Android/app/src/main/java/com/example/svasthya/RegisterActivity.java
@@ -3,8 +3,11 @@ package com.example.svasthya;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,10 +15,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,6 +34,9 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mPassword;
     private Button regBtn;
     private FirebaseAuth mAuth;
+    private int phone;
+    private String IMEI;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +48,7 @@ public class RegisterActivity extends AppCompatActivity {
         mPassword = findViewById(R.id.reg_password);
         regBtn = findViewById(R.id.reg_btn);
         mAuth = FirebaseAuth.getInstance();
-
+        IMEI = getDeviceIMEI();
 
         regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +56,10 @@ public class RegisterActivity extends AppCompatActivity {
                 String name = mName.getText().toString();
                 String password = mPassword.getText().toString();
                 String email = mEmail.getText().toString();
+
+                final Map<String, Object> user = new HashMap<>();
+                user.put("name", name);
+                user.put("IMEI", IMEI);
 
                 if( !name.isEmpty() && !password.isEmpty() && !email.isEmpty()){
 
@@ -52,6 +69,20 @@ public class RegisterActivity extends AppCompatActivity {
                                     if (task.isSuccessful()) {
                                         // Sign in success, update UI with the signed-in user's information
 //                                        Log.d(Tag, "createUserWithEmail:success");
+                                        db.collection("users")
+                                                .add(user)
+                                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                    @Override
+                                                    public void onSuccess(DocumentReference documentReference) {
+//                                                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+//                                                        Log.w(TAG, "Error adding document", e);
+                                                    }
+                                                });
                                         FirebaseUser user = mAuth.getCurrentUser();
                                         Toast.makeText(RegisterActivity.this, "Account created successfully.",
                                                 Toast.LENGTH_SHORT).show();
@@ -77,6 +108,18 @@ public class RegisterActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public String getDeviceIMEI() {
+        String deviceUniqueIdentifier = null;
+        TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        if (null != tm) {
+            deviceUniqueIdentifier = tm.getDeviceId();
+        }
+        if (null == deviceUniqueIdentifier || 0 == deviceUniqueIdentifier.length()) {
+            deviceUniqueIdentifier = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        }
+        return deviceUniqueIdentifier;
     }
 
     public void signIn(View view) {
