2003e6f046bd648ba450633c62918c6f9aa64a9f
Abhilash Saj <abhilashsaj@gmail.com>
2021-02-03 23:25:37 +0530

    conmpleted upload and view documents

--- Android/app/src/main/java/com/example/svasthya/ManualEntryActivity.java
+++ Android/app/src/main/java/com/example/svasthya/ManualEntryActivity.java
@@ -2,31 +2,63 @@ package com.example.svasthya;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import in.gauriinfotech.commons.Commons;
+
 public class ManualEntryActivity extends AppCompatActivity {
 
+    //this is the pic pdf code used in file chooser
+    final static int PICK_PDF_CODE = 2342;
+
+    TextView textViewStatus;
+    EditText editTextFilename;
+    ProgressBar progressBar;
+
+    //the firebase objects for storage and database
+    StorageReference mStorageReference;
+//    DatabaseReference mDatabaseReference;
+
+
+
+    private static final int SELECT_PDF = 0;
     private EditText post_meal_textview;
     private EditText blood_sugar_level_textview;
     private EditText breaths_per_minute_textview;
@@ -53,12 +85,27 @@ public class ManualEntryActivity extends AppCompatActivity {
     private String heart_rate;
     private String cholestorol;
     private String oxygen_saturation;
+    private String SelectedPDF;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_entry);
+
+        mStorageReference = FirebaseStorage.getInstance().getReference();
+//        mDatabaseReference = FirebaseDatabase.getInstance().getReference(Constants.DATABASE_PATH_UPLOADS);
+
+        //getting the views
+        textViewStatus = (TextView) findViewById(R.id.textViewStatus);
+        editTextFilename = (EditText) findViewById(R.id.editTextFileName);
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        //attaching listeners to views
+//        findViewById(R.id.buttonUploadFile).setOnClickListener((View.OnClickListener) this);
+//        findViewById(R.id.textViewUploads).setOnClickListener((View.OnClickListener) this);
+
+
 
         post_meal_textview = (EditText) findViewById(R.id.post_meal);
         blood_sugar_level_textview = (EditText) findViewById(R.id.blood_sugar_level);
@@ -75,7 +122,115 @@ public class ManualEntryActivity extends AppCompatActivity {
 
 
     }
+    private void getPDF() {
+        //for greater than lolipop versions we need the permissions asked on runtime
+        //so if the permission is not available user will go to the screen to allow storage permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+            return;
+        }
 
+        //creating an intent for file chooser
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_PDF_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //when the user choses the file
+        if (requestCode == PICK_PDF_CODE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            //if a file is selected
+            if (data.getData() != null) {
+                //uploading the file
+                uploadFile(data.getData());
+            }else{
+                Toast.makeText(this, "No file chosen", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    //this method is uploading the file
+    //the code is same as the previous tutorial
+    //so we are not explaining it
+    private void uploadFile(Uri data) {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+        final String format = simpleDateFormat.format(new Date());
+
+        progressBar.setVisibility(View.VISIBLE);
+        StorageReference sRef = mStorageReference.child("med_records/" + user.getUid()+"/"+ System.currentTimeMillis() + ".pdf");
+        sRef.putFile(data)
+
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @SuppressWarnings("VisibleForTests")
+                    @Override
+
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        progressBar.setVisibility(View.GONE);
+                        textViewStatus.setText("File Uploaded Successfully");
+                        Toast.makeText(getApplicationContext(), "Successfully uploaded file to storage", Toast.LENGTH_LONG).show();
+
+                        Upload upload = new Upload(editTextFilename.getText().toString(), taskSnapshot.getStorage().getDownloadUrl().toString());
+
+
+                        Map<String, Object> file = new HashMap<>();
+                        file.put("name", upload.getName());
+                        file.put("url", upload.getUrl());
+
+                        db.collection("users").document(user.getUid()).collection("health_records").document(format)
+                                .set(file)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                            Log.d("Success", "DocumentSnapshot successfully written!");
+                                        Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                            Log.w("Fail", "Error writing document", e);
+                                    }
+                                });
+//                        mDatabaseReference.child(mDatabaseReference.push().getKey()).setValue(upload);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @SuppressWarnings("VisibleForTests")
+                    @Override
+                    public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                        textViewStatus.setText((int) progress + "% Uploading...");
+                    }
+                });
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonUploadFile:
+                getPDF();
+                break;
+            case R.id.textViewUploads:
+                startActivity(new Intent(this, ViewUploadsActivity.class));
+                break;
+        }
+    }
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(ManualEntryActivity.this, MainActivity.class));
@@ -157,5 +312,14 @@ public class ManualEntryActivity extends AppCompatActivity {
         }
 
         return deviceId;
+    }
+
+
+    public void uploadFile(View view) {
+        getPDF();
+    }
+
+    public void viewUploads(View view) {
+        startActivity(new Intent(this, ViewUploadsActivity.class));
     }
 }
---
+++ Android/app/src/main/java/com/example/svasthya/ViewUploadsActivity.java
@@ -0,0 +1,124 @@
+package com.example.svasthya;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ViewUploadsActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    //database reference to get uploads data
+    DatabaseReference mDatabaseReference;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    //list to store uploads data
+    List<Upload> uploadList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_uploads);
+
+        uploadList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        //adding a clicklistener on listview
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //getting the upload
+                Upload upload = uploadList.get(i);
+
+                //Opening the upload file in browser using the upload url
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(upload.getUrl()));
+                startActivity(intent);
+            }
+        });
+
+        Map<String, Object> file = new HashMap<>();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        db.collection("users").document(user.getUid()).collection("health_records")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Upload upload = new Upload(document.getString("name"),document.getString("url"));
+                                uploadList.add(upload);
+                                Log.d("Success", document.getId() + " => " + document.getData());
+                                Toast.makeText(getApplicationContext(), "Success " + document.getId() + " => " + document.getData(), Toast.LENGTH_LONG).show();
+                                String[] uploads = new String[uploadList.size()];
+
+                                for (int i = 0; i < uploads.length; i++) {
+                                    uploads[i] = uploadList.get(i).getName();
+                                }
+
+                                //displaying it to list
+                                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+                                listView.setAdapter(adapter);
+                            }
+                        } else {
+                            Log.w("Fail", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
+
+        //retrieving upload data from firebase database
+//        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                    Upload upload = postSnapshot.getValue(Upload.class);
+//                    uploadList.add(upload);
+//                }
+//
+//                String[] uploads = new String[uploadList.size()];
+//
+//                for (int i = 0; i < uploads.length; i++) {
+//                    uploads[i] = uploadList.get(i).getName();
+//                }
+//
+//                //displaying it to list
+//                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+//                listView.setAdapter(adapter);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+    }
+}
