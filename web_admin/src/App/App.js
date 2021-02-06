a02729ad392d7f04c3b859192d76762e764d240b
Abhilash Saj <abhilashsaj@gmail.com>
2021-02-06 20:30:40 +0530

    side-menu

---
+++ web_admin/src/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import SideMenu from '../components/SideMenu';
+
+
+
+// <> means <React.Fragment>
+function App() {
+  return (
+    <>
+    <div><SideMenu/></div>
+    
+    </>
+  );
+}
+
+export default App;
