017a7ee5e6b99f85dd1681a99c7d38bc06210ad1
Abhilash Saj <abhilashsaj@gmail.com>
2021-02-07 18:19:04 +0530

    added header

--- web_admin/src/App/App.js
+++ web_admin/src/App/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import SideMenu from '../components/SideMenu';
+import { makeStyles, CssBaseline } from '@material-ui/core';
+import Header from '../components/Header';
 
 
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  }
+})
 
 // <> means <React.Fragment>
 function App() {
+  const classes = useStyles();
   return (
     <>
-    <div><SideMenu/></div>
-    
+      <SideMenu/>
+      <div className={classes.appMain}>
+        <Header/>
+      </div>
+      <CssBaseline/>
     </>
   );
 }
