c483a86e1813274f78c3379a863213cf11a59ac0
Abhilash Saj <abhilashsaj@gmail.com>
2021-02-09 19:08:23 +0530

    added 5 min meditation

--- Android/app/src/main/java/com/example/svasthya/StressHomeActivity.java
+++ Android/app/src/main/java/com/example/svasthya/StressHomeActivity.java
@@ -2,7 +2,9 @@ package com.example.svasthya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StressHomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class StressHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stress_home);
+    }
+
+    public void five_min_meditation(View view) {
+        Intent intent = new Intent(StressHomeActivity.this, fi.class);
+        startActivity(intent);
     }
 }
