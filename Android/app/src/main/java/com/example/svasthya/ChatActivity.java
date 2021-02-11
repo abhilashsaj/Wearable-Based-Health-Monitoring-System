107a072160fb148dd14d383929f7b4bc3263d0aa
Abhilash Saj <abhilashsaj@gmail.com>
2021-02-12 00:40:28 +0530

    completed chatbot

--- Android/app/src/main/java/com/example/svasthya/ChatActivity.java
+++ Android/app/src/main/java/com/example/svasthya/ChatActivity.java
@@ -59,10 +59,11 @@ public class ChatActivity extends AppCompatActivity {
 
             public void onClick(View v) {
 
-                final String message = editText.getText().toString().trim();
+                final String message = editText.getText().toString().trim().toLowerCase();
 
 
                 if (!message.equals("")) {
+                    msg_builder = "";
                     if(message.equals("files")){
                         db.collection("user_health_record")
                                 .whereEqualTo("UID", currentUser.getUid())
@@ -73,7 +74,7 @@ public class ChatActivity extends AppCompatActivity {
                                         if (task.isSuccessful()) {
                                             for (QueryDocumentSnapshot document : task.getResult()) {
                                                 Log.d("Chat Activity", document.getId() + " => " + document.getData());
-                                                Toast.makeText(ChatActivity.this, document.getData().toString(), Toast.LENGTH_SHORT).show();
+//                                                Toast.makeText(ChatActivity.this, document.getData().toString(), Toast.LENGTH_SHORT).show();
                                                 msg_builder = msg_builder + document.getString("url")+":"+ document.getString("url")+"\n";
                                                 ChatMessage chatMessage = new ChatMessage(msg_builder, message);
                                                 dataModels.add(chatMessage);
@@ -81,12 +82,51 @@ public class ChatActivity extends AppCompatActivity {
                                             }
                                         } else {
                                             Log.d("Chat Activity", "Error getting documents: ", task.getException());
+                                            ChatMessage chatMessage = new ChatMessage("Not found", message);
+                                            dataModels.add(chatMessage);
+                                            adapter.notifyDataSetChanged();
                                         }
                                     }
                                 });
                     }
-                    else if(message.equals("files"){
-                        db.collection("")
+                    else if(message.contains("file")){
+
+                        String[] arrOfStr = message.split(" ", 2);
+                        msg_builder= "";
+                        Toast.makeText(ChatActivity.this, arrOfStr[1], Toast.LENGTH_SHORT).show();
+
+                        db.collection("user_health_record")
+                                .whereEqualTo("UID", currentUser.getUid())
+                                .whereEqualTo("name", arrOfStr[1])
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Log.d("Chat Activity", document.getId() + " => " + document.getData());
+//                                                msg_builder = document.getData().toString();
+//                                                msg_builder= msg_builder.substring(1, msg_builder.length() - 1);
+//                                                msg_builder = msg_builder.replace(", ", "\n");
+                                                msg_builder += document.getString("url")+"\n";
+
+                                            }
+                                            ChatMessage chatMessage = new ChatMessage(msg_builder, message);
+                                            dataModels.add(chatMessage);
+                                            adapter.notifyDataSetChanged();
+                                        } else {
+                                            Log.d("Chat Activity", "Error getting documents: ", task.getException());
+                                            ChatMessage chatMessage = new ChatMessage("Not found", message);
+                                            dataModels.add(chatMessage);
+                                            adapter.notifyDataSetChanged();
+                                        }
+                                    }
+                                });
+
+                    }
+                    else if(message.contains("status")){
+                        msg_builder= "";
+                        db.collection("user_health")
                                 .whereEqualTo("UID", currentUser.getUid())
                                 .get()
                                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -95,14 +135,75 @@ public class ChatActivity extends AppCompatActivity {
                                         if (task.isSuccessful()) {
                                             for (QueryDocumentSnapshot document : task.getResult()) {
                                                 Log.d("Chat Activity", document.getId() + " => " + document.getData());
-                                                Toast.makeText(ChatActivity.this, document.getData().toString(), Toast.LENGTH_SHORT).show();
-                                                msg_builder = msg_builder + document.getString("url")+":"+ document.getString("url")+"\n";
-                                                ChatMessage chatMessage = new ChatMessage(msg_builder, message);
+                                                String obj_string = document.getData().toString();
+                                                obj_string = obj_string.substring(1, obj_string.length() - 1).replace(", ", "\n");
+                                                msg_builder += obj_string +"\n";
+
+                                            }
+                                            ChatMessage chatMessage = new ChatMessage(msg_builder, message);
+                                            dataModels.add(chatMessage);
+                                            adapter.notifyDataSetChanged();
+                                        } else {
+                                            ChatMessage chatMessage = new ChatMessage("Not found", message);
+                                            dataModels.add(chatMessage);
+                                            adapter.notifyDataSetChanged();
+                                        }
+                                    }
+                                });
+
+                    }
+                    else if(message.toLowerCase().contains("bp")){
+                        msg_builder= "";
+                        db.collection("user_health")
+                                .whereEqualTo("UID", currentUser.getUid())
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Log.d("Chat Activity", document.getId() + " => " + document.getData());
+                                                ChatMessage chatMessage = new ChatMessage("BP (DIA): "+document.getString("blood_pressure_dia") +
+                                                        "\nBP (SYS): " + document.getString("blood_pressure_sys"), message);
                                                 dataModels.add(chatMessage);
                                                 adapter.notifyDataSetChanged();
+
                                             }
+
                                         } else {
                                             Log.d("Chat Activity", "Error getting documents: ", task.getException());
+                                            ChatMessage chatMessage = new ChatMessage("Not found", message);
+                                            dataModels.add(chatMessage);
+                                            adapter.notifyDataSetChanged();
+                                        }
+                                    }
+                                });
+
+                    }else if(message.equals("hi")){
+                        //  /users/7nphycPvVaboW2F7Ha7oB364zPD2/health_data/05-02-2021/time_slot/03-15-19
+
+                        db.collection("users/7nphycPvVaboW2F7Ha7oB364zPD2/health_data/05-02-2021/time_slot/")
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Log.d("Chat Activity", document.getId() + " => " + document.getData());
+//                                                ChatMessage chatMessage = new ChatMessage("BP (DIA): "+document.getString("blood_pressure_dia") +
+//                                                        "\nBP (SYS): " + document.getString("blood_pressure_sys"), message);
+//                                                dataModels.add(chatMessage);
+//                                                adapter.notifyDataSetChanged();
+                                                Toast.makeText(ChatActivity.this, document.getData().toString(), Toast.LENGTH_LONG);
+
+                                            }
+
+                                        } else {
+                                            Log.d("Chat Activity", "Error getting documents: ", task.getException());
+//                                            Toast.makeText(ChatActivity.this, document.getData().toString(), Toast.LENGTH_LONG);
+                                            ChatMessage chatMessage = new ChatMessage("Not found", message);
+                                            dataModels.add(chatMessage);
+                                            adapter.notifyDataSetChanged();
                                         }
                                     }
                                 });
